f8ad6480d552c67f0b83e806a65a4812ea3923f7
Victor Polyakov <polyakov.victor21@gmail.com>
2024-02-26 06:40:54 +0200

    back/ edit endpoints

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+import time
 
 
 app = FastAPI()
@@ -16,11 +17,16 @@ app.add_middleware(CORSMiddleware,
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[int, WebSocket] = {}
+
+    async def create(self, websocket: WebSocket):
+        chat_id = str(int(time.time() * 1000))
+        await websocket.accept()
+        self.active_connections[chat_id] = websocket
+        return chat_id
 
     async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+        print("CREATE NEW WEBSOCKET", 'id:', id)
 
     def disconnect(self, websocket: WebSocket):
         print('DISCONNECT USER')
@@ -37,14 +43,20 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id):
+@app.websocket("/ws/{chat_id}")
+async def websocket_endpoint(websocket: WebSocket, chat_id):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client #{chat_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} has left the chat")
+        await manager.broadcast(f"Client #{chat_id} has left the chat")
+
+
+@app.get("/create_chat/")
+async def create_chat():
+    chat_id = manager.create()
+    return {"chat_id": chat_id}
